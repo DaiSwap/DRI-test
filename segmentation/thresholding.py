b9e151010f6cee9e1c0d142e6b4b50d1479a8e31
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-07-05 22:50:26 +0530

    modified

--- segmentation/thresholding.py
+++ segmentation/thresholding.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Load the image in grayscale
 image_path = 'segmentation\simple2.jpg'
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-"""
+
 # Apply manual thresholding
 threshold_value = 190  # Adjust this value based on your needs
 _, binary = cv2.threshold(image, threshold_value, 255, cv2.THRESH_BINARY)
@@ -19,8 +19,8 @@ plt.subplot(1, 2, 2)
 plt.title("Binary Segmentation")
 plt.imshow(binary, cmap='gray')
 plt.show()
-"""
 
+"""
 # Apply Otsu's thresholding
 _, binary_otsu = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
@@ -33,7 +33,7 @@ plt.subplot(1, 2, 2)
 plt.title("Otsu's Binary Segmentation")
 plt.imshow(binary_otsu, cmap='gray')
 plt.show()
-
+"""
 """
 # Apply adaptive thresholding
 binary_adaptive = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
